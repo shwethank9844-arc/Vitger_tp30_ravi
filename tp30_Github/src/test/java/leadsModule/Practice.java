af0ab47d08a46a18bcc1451aa9ecc7755d015daf
ravi1 <ravi1@LAPTOP-4QN7VMLV>
2025-12-29 14:51:24 +0530

    hi

--- tp30_Github/src/test/java/leadsModule/Practice.java
+++ tp30_Github/src/test/java/leadsModule/Practice.java
@@ -1,12 +1,15 @@
 package leadsModule;
 
+import java.io.File;
+
+import org.openqa.selenium.OutputType;
 import org.testng.annotations.Test;
 
 public class Practice {
 
-	
 	@Test
-	public void pract() {
+	public void pract() 
+	{
 		
 		System.out.println(" -- merging -- ");
 		System.out.println(" -- merging -- ");
